3db03adf4e88313a82f1d31de9597a0568c72f7d
Artem <90392848+TomiUdras@users.noreply.github.com>
2022-06-27 18:41:13 +0300

    more3

--- source/test.cpp
+++ source/test.cpp
@@ -5,20 +5,8 @@ TEST_CASE("test1"){
     REQUIRE(book::search('qwer','qwer') == 1);
 }
 TEST_CASE("test2"){
-    REQUIRE(book::search('qwer','qwer') == 1);
+    REQUIRE(book::search('qweryui','qwer') == 0);
 }
 TEST_CASE("test3"){
-    REQUIRE(book::search('qwer','qwer') == 1);
-}
-TEST_CASE("test4"){
-    REQUIRE(book::search('qwer','qwer') == 1);
-}
-TEST_CASE("test5"){
-    REQUIRE(book::search('qwer','qwer') == 1);
-}
-TEST_CASE("test6"){
-    REQUIRE(book::search('qwer','qwer') == 1);
-}
-TEST_CASE("test7"){
-    REQUIRE(book::search('qwer','qwer') == 1);
+    REQUIRE(book::search('qwer','qoiuywer') == 0);
 }
