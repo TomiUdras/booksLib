f21d00b8974e673d6896eb6517d7364914ecb89e
Artem <90392848+TomiUdras@users.noreply.github.com>
2022-06-29 13:25:16 +0300

    more1

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /Users/tomiudras/CLionProjects/booksLib
+# Build directory: /Users/tomiudras/CLionProjects/booksLib/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test([=[Test]=] "Test" "—force-colors" "-d")
+set_tests_properties([=[Test]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/tomiudras/CLionProjects/booksLib/CMakeLists.txt;17;add_test;/Users/tomiudras/CLionProjects/booksLib/CMakeLists.txt;0;")
+subdirs("external/doctest")
---
+++ cmake-build-debug/external/doctest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/tomiudras/CLionProjects/booksLib/external/doctest
+# Build directory: /Users/tomiudras/CLionProjects/booksLib/cmake-build-debug/external/doctest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
